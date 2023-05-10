5b2efc23b3e212010e5b09deebdc2061fc262943
Kristoff-Villanueva <kristoffer.villanueva18@gmail.com>
2023-05-10 23:40:02 +0800

    add redux middleware

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from "react";
 // import ReactDOM from 'react-dom'; The new way to import createRoot:
 import { createRoot } from "react-dom/client";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
 import "./index.css";
 import App from "./containers/App";
 import "tachyons";
 import { Provider } from "react-redux";
-import { legacy_createStore as createStore } from "redux";
-import { searchRobots } from "./reducers";
+import {
+	legacy_createStore as createStore,
+	applyMiddleware,
+	combineReducers,
+} from "redux";
+import { searchRobots, requestRobots } from "./reducers";
 // import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+
+const store = createStore(
+	rootReducer,
+	applyMiddleware(thunkMiddleware, logger)
+);
 
 const root = createRoot(document.getElementById("root"));
 root.render(
