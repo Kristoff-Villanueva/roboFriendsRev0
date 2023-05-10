ef969259f66c23baef6da3d933231e8926c5e9cb
Kristoff-Villanueva <kristoffer.villanueva18@gmail.com>
2023-05-10 15:18:47 +0800

    fix reducer and refactor app.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import "./App.css";
 
+import { setSearchField } from "../actions";
+
+const mapStateToProps = (state) => {
+	return {
+		searchField: state.searchField,
+	};
+};
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+	};
+};
+
 function App(props) {
 	const [robots, setRobots] = useState([]);
-	const [searchfield, setSearchfield] = useState("");
 	const [count, setCount] = useState(0); // for demo purposes
+	const { onSearchChange, searchField } = props;
 
 	useEffect(() => {
 		fetch("https://jsonplaceholder.typicode.com/users")
@@ -18,12 +33,8 @@ function App(props) {
 		// console.log(count)
 	}, []); // if you add count, only run if count changes.
 
-	const onSearchChange = (event) => {
-		setSearchfield(event.target.value);
-	};
-
 	const filteredRobots = robots.filter((robot) => {
-		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+		return robot.name.toLowerCase().includes(searchField.toLowerCase());
 	});
 
 	return !robots.length ? (
@@ -42,4 +53,4 @@ function App(props) {
 	);
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
